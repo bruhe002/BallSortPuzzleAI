d5120a120e7030eb12ddc14420967ab357d28ef4
Eric Bruhns <bruhe002@gmail.com>
2023-11-16 19:59:06 -0700

    Testing out different Assets

--- BallSortPuzzle.py
+++ BallSortPuzzle.py
@@ -1,4 +1,21 @@
 
 
 import pygame
+import sys
 
+pygame.init()
+
+screen = pygame.display.set_mode((1080, 720))
+clock = pygame.time.Clock()
+
+# # Acquire background image
+bg_surface = pygame.image.load('assets/bg-game-asset.jpg').convert()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    screen.blit(bg_surface, (0,0))
